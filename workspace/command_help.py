3def626b51b5fbce604e96726df998b9546fc68f
Gregwar <g.passault@gmail.com>
2021-06-24 15:32:38 +0200

    Showing version in help

--- workspace/command_help.py
+++ workspace/command_help.py
@@ -1,5 +1,6 @@
 from colorama import Fore, Back, Style
 from workspace import commands, message
+import pkg_resources  # part of setuptools
 
 name = "help"
 help = """Provides help about commands"""
@@ -7,13 +8,14 @@ usage = ""
 min_args = 0
 
 def run(args):
-  print('Available commands:')
-  print('')
+    print('wks version %s' % pkg_resources.require("wks")[0].version)
+    print('Available commands:')
+    print('')
 
-  for entry in commands.commands:
-    message.bright(message.emphasis("wks") + " " + entry.name + ' ' + Style.RESET_ALL + entry.usage)
-    lines = entry.help.strip().split("\n")
-    lines = ["    " + line for line in lines]
-    print("\n".join(lines))
+    for entry in commands.commands:
+        message.bright(message.emphasis("wks") + " " + entry.name + ' ' + Style.RESET_ALL + entry.usage)
+        lines = entry.help.strip().split("\n")
+        lines = ["    " + line for line in lines]
+        print("\n".join(lines))
 
-  print('')
+    print('')
