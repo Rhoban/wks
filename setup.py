de8e311aeb9b0d10712aae2848aabbf2441efce9
Gregwar <g.passault@gmail.com>
2022-04-13 16:26:25 +0200

    0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="wks",
-    version="0.2.0",
+    version="0.2.1",
     author="Rhoban team",
     author_email="team@rhoban.com",
     description="Simple dependencies manager for cmake projects in your workspace",
