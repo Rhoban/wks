9f6e3e808f63fea8288da73d5a87070680a8a721
Gregwar <g.passault@gmail.com>
2022-04-15 14:10:02 +0200

    Adding cmd to run a command in all the directories

---
+++ workspace/command_cmd.py
@@ -0,0 +1,15 @@
+import os
+import numpy as np
+from workspace import env, git, message, cmake
+
+name = "cmd"
+help = """Run a command in all dependencies"""
+usage = "[command]"
+min_args = 1
+
+
+def run(args):
+    command = ' '.join(args)
+    for directory in git.get_directories():
+        message.bright("* Running command in %s" % directory)
+        os.system("cd %s; %s" % (directory, command))
--- workspace/commands.py
+++ workspace/commands.py
@@ -1,5 +1,19 @@
 import sys
 
-from workspace import command_build, command_cmake, command_help, command_install, command_pull, command_status, command_graph
+from workspace import (
+    command_build,
+    command_cmake,
+    command_help,
+    command_install,
+    command_pull,
+    command_status,
+    command_graph,
+    command_cmd,
+)
 
-commands = [command_build, command_cmake, command_help, command_install, command_pull, command_status, command_graph]
+# Adding all imported commands to the commands list
+commands = []
+entries = locals().copy()
+for name in entries:
+    if name.startswith('command_'):
+        commands.append(entries[name])
