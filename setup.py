429b984b23073f946a96e5caf7f7f333c0585367
Grégoire Passault <g.passault@gmail.com>
2025-05-23 21:29:54 +0200

    0.4.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="wks",
-    version="0.4.5",
+    version="0.4.6",
     author="Rhoban team",
     author_email="team@rhoban.com",
     description="Simple dependencies manager for cmake projects in your workspace",
