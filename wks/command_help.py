a7f5289be675e976260090444067f8927d8a2a38
Grégoire Passault <g.passault@gmail.com>
2026-04-01 13:43:45 +0200

    0.5.2

--- wks/command_help.py
+++ wks/command_help.py
@@ -1,6 +1,5 @@
 from colorama import Fore, Back, Style
 from . import commands, message
-import pkg_resources  # part of setuptools
 
 name = "help"
 help = """Provides help about commands"""
@@ -9,10 +8,6 @@ min_args = 0
 
 
 def run(args):
-    try:
-        print("wks version %s" % pkg_resources.require("wks")[0].version)
-    except pkg_resources.DistributionNotFound:
-        print("wks version DEV")
     print("Available commands:")
     print("")
 
